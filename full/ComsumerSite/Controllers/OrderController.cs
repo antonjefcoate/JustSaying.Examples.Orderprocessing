4b5be4e1efa0819a1616c623164ffcb50cb019d5
Anton <anton.jefcoate@just-eat.com>
2014-04-29 17:16:58 +0100

    First working example

---
+++ full/ComsumerSite/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComsumerSite.Models;
+using JustSaying.Messaging;
+using Messages;
+using Messages.Commands;
+
+namespace ComsumerSite.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IMessagePublisher _publisher;
+
+        public OrderController()
+        {
+            _publisher = JustSaying.CreateMe.ABus(config =>
+            {
+                config.Region = "eu-west-1";
+            })
+                .WithSnsMessagePublisher<PlaceOrder>(Constants.OrderProcessingTopic);
+
+        }
+
+        //
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(OrderDetails orderDetails)
+        {
+            _publisher.Publish(new PlaceOrder(new List<string>{orderDetails.Item1, orderDetails.Item2}, orderDetails.Price, orderDetails.CustomerName));
+            return View("Placed");
+        }
+
+	}
+}
--- full/OrderProcessor/Program.cs
+++ full/OrderProcessor/Program.cs
@@ -1,4 +1,5 @@
-﻿using Messages;
+﻿using System;
+using Messages;
 
 namespace JustSaying.Examples.OrderProcessing.OrderProcessor
 {
@@ -9,14 +10,21 @@ namespace JustSaying.Examples.OrderProcessing.OrderProcessor
             var bus = CreateMe.ABus(config =>
             {
                 config.Region = "eu-west-1";
-            })
+            })  
+                // Subscribe to order processing topic
                 .WithSqsTopicSubscriber(config =>
                 {
                     config.Topic = Constants.OrderProcessingTopic;
                     config.MessageRetentionSeconds = 120;
                 });
             
+            // Add a handler for the place order command
             bus.WithMessageHandler(new OrderPlacement(bus));
+
+            // Start listening for messages
+            bus.StartListening();
+
+            Console.Read();
         }
     }
 }
