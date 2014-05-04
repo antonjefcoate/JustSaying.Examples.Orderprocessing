171a7f5da545f9cd126b1c8d42fb3dbe15f43762
Payman Labbaf <payman.labbaf@just-eat.com>
2014-05-04 12:02:41 +0100

    Communicate rejected/accepted orders to consumers on order confirm page.

--- full/ComsumerSite/Global.asax.cs
+++ full/ComsumerSite/Global.asax.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using JustSaying.Messaging;
+using Messages;
+using Messages.Commands;
+using Messages.Events;
 
 namespace ComsumerSite
 {
@@ -12,6 +16,18 @@ namespace ComsumerSite
     {
         protected void Application_Start()
         {
+            var dispatcher = new OrderResolutionDispatcher();
+            JustSaying.CreateMeABus.InRegion("eu-west-1")
+                .WithSqsTopicSubscriber(Constants.RestaurantOrdersTopic)
+                .IntoQueue("OrderResolution")
+                .ConfigureSubscriptionWith(config =>
+                {
+                    config.MessageRetentionSeconds = 120;
+                })
+                .WithMessageHandler<OrderAccepted>(dispatcher)
+                .WithMessageHandler<OrderRejected>(dispatcher)
+                .StartListening();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
